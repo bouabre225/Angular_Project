c5b4fa73a4be1d61756654fc67e6462f3aacedb5
FloriaNE229 <rebeccahessouh@outlook.fr>
2025-07-24 10:36:04 +0900

    modif

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { DeplacementDesRecettes } from './body/deplacement-des-recettes/deplacement-des-recettes';
+import { VueDeSemaine } from './body/vue-de-semaine/vue-de-semaine';
+import { SuppressionRapide } from './body/suppression-rapide/suppression-rapide';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, DeplacementDesRecettes, VueDeSemaine, SuppressionRapide],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
