e5e565ee367692a7278534c2718cef88c5ba960e
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-24 10:07:01 +0200

    modif

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecetteService } from './recette';
+import { ListeRecette } from './liste-recette/liste-recette';
+import { CommonModule } from '@angular/common';
+import { DetailRecette } from './detail-recette/detail-recette';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AjoutRecette } from './ajout-recette/ajout-recette';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListeRecette, CommonModule, DetailRecette, RouterModule, FormsModule, AjoutRecette],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  template: `<router-outlet></router-outlet>`
 })
 export class App {
-  protected readonly title = signal('Recette_Project');
+  title = signal('Recette_Project');
+
+
+  constructor(public recetteService: RecetteService) {}
 }
+
