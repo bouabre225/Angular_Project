9374d3202029d6612fefe9ee0e4abdb749d61199
Florent BOUDZOUMOU <109081275+Florent242@users.noreply.github.com>
2025-07-24 18:05:14 +0200

    retrait des imports plus necessaires car inutitsee en fonctions des changements apportees par les redirections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,44 +1,47 @@
-import { Routes } from '@angular/router';
-import { ListeRecette } from './liste-recette/liste-recette';
-import { DetailRecette } from './detail-recette/detail-recette';
+import type { Routes } from "@angular/router"
 
 export const routes: Routes = [
   {
-    path: '',
-    loadComponent: () => import('./components/liste-recette/liste-recette')
-      .then(m => m.ListeRecetteComponent),
-    title: 'Mes Recettes'
+    path: "",
+    loadComponent: () => import("./components/liste-recette/liste-recette").then((m) => m.ListeRecetteComponent),
+    title: "Mes Recettes",
   },
   {
-    path: 'recette/new',
-    loadComponent: () => import('./components/recette-formulaire/recette-formulaire')
-      .then(m => m.RecetteFormulaireComponent),
-    title: 'Nouvelle Recette'
+    path: "recette/new",
+    loadComponent: () =>
+      import("./components/recette-formulaire/recette-formulaire").then((m) => m.RecetteFormulaireComponent),
+    title: "Nouvelle Recette",
   },
   {
-    path: 'recette/edit/:id',
-    loadComponent: () => import('./components/recette-formulaire/recette-formulaire')
-      .then(m => m.RecetteFormulaireComponent),
-    title: 'Modifier Recette'
+    path: "recette/edit/:id",
+    loadComponent: () =>
+      import("./components/recette-formulaire/recette-formulaire").then((m) => m.RecetteFormulaireComponent),
+    title: "Modifier Recette",
   },
   {
-    path: 'recette/:id',
-    loadComponent: () => import('./components/details-recette/details-recette')
-      .then(m => m.DetailsRecetteComponent),
-    title: 'Détail Recette'
+    path: "recette/:id",
+    loadComponent: () => import("./components/details-recette/details-recette").then((m) => m.DetailsRecetteComponent),
+    title: "Détail Recette",
   },
   {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full'
+    path: "deplacement",
+    loadComponent: () =>
+      import("./body/deplacement-des-recettes/deplacement-des-recettes").then((m) => m.DeplacementDesRecettes),
+    title: "Déplacement des Recettes",
   },
-  { path: '', 
-    component: ListeRecette 
+  {
+    path: "suppression",
+    loadComponent: () => import("./body/suppression-rapide/suppression-rapide").then((m) => m.SuppressionRapide),
+    title: "Suppression Rapide",
   },
-    { path: 'recette/:id', 
-    component: DetailRecette 
+  {
+    path: "planning",
+    loadComponent: () => import("./body/vue-de-semaine/vue-de-semaine").then((m) => m.VueDeSemaine),
+    title: "Vue de Semaine",
   },
-    { path: '**', 
-    redirectTo: '' 
-  }
-];
+  {
+    path: "**",
+    redirectTo: "",
+    pathMatch: "full",
+  },
+]
