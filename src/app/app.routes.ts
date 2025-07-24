c8f2949b54bd566558ba143245e54909643e191d
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-24 11:28:37 +0200

    modification des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { ListeRecette } from './liste-recette/liste-recette';
 import { DetailRecette } from './detail-recette/detail-recette';
-import { AjoutRecette } from './ajout-recette/ajout-recette';
 
 export const routes: Routes = [
     { path: '', component: ListeRecette },
     { path: 'recette/:id', component: DetailRecette },
-    { path: 'ajout-recette', component: AjoutRecette },
     { path: '**', redirectTo: '' }
 ];
