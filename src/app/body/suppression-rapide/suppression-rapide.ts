d0372cf0d52164fda248c39c28ebeff3ebd70a22
FloriaNE229 <rebeccahessouh@outlook.fr>
2025-07-24 10:34:19 +0900

    ajout

---
+++ src/app/body/suppression-rapide/suppression-rapide.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suppression-rapide',
+  imports: [],
+  templateUrl: './suppression-rapide.html',
+  styleUrl: './suppression-rapide.css'
+})
+export class SuppressionRapide {
+
+}
