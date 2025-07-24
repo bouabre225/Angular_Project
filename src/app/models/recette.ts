e15d3a0bfef379e3ade761ec6c4bd5e1e1262536
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-24 19:53:20 +0200

    modification

--- src/app/models/recette.ts
+++ src/app/models/recette.ts
@@ -6,6 +6,8 @@ export interface Recette {
     cuisson: number;
     difficulte: 'Facile' | 'Moyenne' | 'Difficile';
     categorie: 'Entrée' | 'Plat' | 'Dessert';
+    jour: 'Lundi' | 'Mardi' | 'Mercredi' | 'Jeudi' | 'Vendredi' | 'Samedi' | 'Dimanche';
+    type: 'Petit-déjeuner' | 'Déjeuner' | 'Dîner' | 'Dessert';
     ingredients: string[];
     preparation: string[];
     nutrition?: string;
